29a7c70a6ddf3ca817b3fdfad7db895483c00d8e
raul far <raulfarllobell@gmail.com>
2024-03-10 14:46:00 +0100

    refactor: styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="text-primary bg-primary z-50 text-2xl  bottom-0 w-full fixed">
+    <footer className="text-primary bg-primary z-50 text-2xl bottom-0 w-full fixed">
       <div className="mx-auto container flex justify-center py-6">
         <p className="text-center">©2024</p>
       </div>
